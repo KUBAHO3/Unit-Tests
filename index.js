a4f081b97ff934de5ec3b7319757d0d3dced1734
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-04 22:49:30 +0530

    task 1 part 2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function stringLength (string) {
-    return string.length;
+    if (string.length < 1 || string.length > 10){
+        return string.length;
+    } else {
+        return 'number > 10 0r < 1'
+    }
 }
 
-// console.log(stringLength('minecaft'));
 
-if (stringLength('minecraft') !== 8 ){
-    throw new Error('the test failed');
-} else {
+if (stringLength('minecraft') === 9 ){
     console.log('the test passed');
+} else {
+    throw new Error('the test failed');
+}
+
+if (stringLength('') === 'number > 10 0r < 1' ){
+    console.log('the test passed');
+} else {
+    throw new Error('the test failed');
+}
+
+if (stringLength('minecrafting always') === 'number > 10 0r < 1' ){
+    console.log('the test passed');
+} else {
+    throw new Error('the test failed');
 }
